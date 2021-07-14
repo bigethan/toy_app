c462f11140bacec758ae1da78176fec8085cb13a
Ethan Schlenker <e@bigethan.com>
2021-07-14 11:56:14 -0700

    Get user show view to include user's microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    set_user
+    @microposts = Micropost.where({user_id: @user.id})
   end
 
   # GET /users/new
